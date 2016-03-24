abe3e611c149de894853c970be93e2e0bd794d7b
Wilder <wildermachadodacruz@gmail.com>
2016-03-24 11:33:27 -0300

    Alteração do layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,13 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/html">
 
 <head>
-
-
     <title>ADS</title>
     {!! Html::style('css/normalize.css') !!}
     {!! Html::style('css/demo.css') !!}
@@ -14,21 +17,25 @@
     {!! Html::style('css/bootstrap-select.css') !!}
     {!! Html::style('css/style.css') !!}
 
+
             <!-- for-mobile-apps -->
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="keywords" content="Resale Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design"/>
 
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <script type="application/x-javascript"> addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        } </script>
 
     <!-- //for-mobile-apps -->
     <!--fonts-->
     {!! Html::style('//fonts.googleapis.com/css?family=Ubuntu+Condensed') !!}
     {!! Html::style('//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic') !!}
             <!--//fonts-->
-
 
     <!-- js -->
     {!! Html::script('js/jquery.min.js') !!}
@@ -74,15 +81,15 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     {!! Html::script('js/jquery.uls.core.js') !!}
 
     <script>
-        $( document ).ready( function() {
-            $( '.uls-trigger' ).uls( {
-                onSelect : function( language ) {
-                    var languageName = $.uls.data.getAutonym( language );
-                    $( '.uls-trigger' ).text( languageName );
+        $(document).ready(function () {
+            $('.uls-trigger').uls({
+                onSelect: function (language) {
+                    var languageName = $.uls.data.getAutonym(language);
+                    $('.uls-trigger').text(languageName);
                 },
                 quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
-            } );
-        } );
+            });
+        });
     </script>
 
     {!! Html::style('css/easy-responsive-tabs.css') !!}
@@ -100,6 +107,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             <a class="account" href="login.html">Gerenciar Conta</a>
             <span class="active uls-trigger">Gerenciar Semestre</span>
             <!-- Large modal -->
+
         </div>
     </div>
 </div>
@@ -107,8 +115,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <nav id="menuHeader">
     <div class="banner text-center">
         <div class="container">
-            <h1>Instituto Federal de Educação, Ciência e   <span class="segment-heading">    Tecnologia da Bahia </span>Campus Eunápolis</h1>
-            <p>Curso Tecnologia em Análise e Desenvolvimento de Sistemas</p>
+            <h1>Instituto Federal de Educação, Ciência e <span class="segment-heading">    Tecnologia da Bahia </span>
+                Campus Eunápolis</h1>
+
+            <p> Curso Tecnologia em Análise e Desenvolvimento de Sistemas</p>
             <a href="post-ad.html">Publicar Aviso</a>
         </div>
     </div>
@@ -119,34 +129,74 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <div class="categories-section main-grid-border">
     <div class="container">
         <h2 class="head">Menu Categorias</h2>
+
         <div class="category-list">
             <div id="parentVerticalTab">
                 <ul class="resp-tabs-list hor_1">
-                    <li><a>Avaliação</a></li>
+                    <li>Avaliação</li>
                     <li><a href="disciplinas">Disciplinas</a></li>
                     <li><a href="professores">Docentes</a></li>
-                    <li><a>Documentos</a></li>
+                    <li>Documentos</li>
                     <a href="post-ad.html">Consultar Avisos</a>
                 </ul>
                 <div class="resp-tabs-container hor_1">
-                    <span class="active total" style="display:block;" ><strong>PAINEL DE CONTROLE</strong> </span>
-                    @if($errors->any())
-                        <ul class="alert alert-warning">
-                            @foreach($errors->all() as $error)
-                                <li>{{$error}}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                        @yield('conteudo')
+                    <span class="active total" style="display:block;"><strong>PAINEL DE CONTROLE</strong> </span>
+                    <div>
+                    @yield('conteudo')
+                    </div>
+
+                    <div>
+                        <div class="category">
+                            <div class="category-img">
+                                <img src="images/cat2.png" title="image" alt=""/>
+                            </div>
+                            <div class="category-info">
+                                <h4>Electronics & Appliances</h4>
+                                <span>2,01,850 Ads</span>
+                                <a href="all-classifieds.html">View all Ads</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="sub-categories">
+                            <ul>
+                                <li><a href="electronics-appliances.html">Computers & accessories</a></li>
+                                <li><a href="electronics-appliances.html">Tv - video - audio</a></li>
+                                <li><a href="electronics-appliances.html">Cameras & accessories</a></li>
+                                <li><a href="electronics-appliances.html">Games & Entertainment</a></li>
+                                <li><a href="electronics-appliances.html">Fridge - AC - Washing Machine</a></li>
+                                <li><a href="electronics-appliances.html">Kitchen & Other Appliances</a></li>
+                                <div class="clearfix"></div>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="category">
+                            <div class="category-img">
+                                <img src="images/cat3.png" title="image" alt=""/>
+                            </div>
+                            <div class="category-info">
+                                <h4>Cars</h4>
+                                <span>1,98,080 Ads</span>
+                                <a href="all-classifieds.html">View all Ads</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="sub-categories">
+                            @yield('disciplinas.index')
+                        </div>
+                    </div>
+                    <div>
+                    @yield('documentos.index')
+                    </div>
                 </div>
+                <div class="clearfix"></div>
             </div>
         </div>
     </div>
 </div>
-
 <!--Plug-in Initialisation-->
 <script type="text/javascript">
-    $(document).ready(function() {
+    $(document).ready(function () {
 
         //Vertical Tab
         $('#parentVerticalTab').easyResponsiveTabs({
@@ -155,7 +205,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             fit: true, // 100% fit in a container
             closed: 'accordion', // Start closed if in accordion view
             tabidentify: 'hor_1', // The tab groups identifier
-            activate: function(event) { // Callback function if tab is switched
+            activate: function (event) { // Callback function if tab is switched
                 var $tab = $(this);
                 var $info = $('#nested-tabInfo2');
                 var $name = $('span', $info);
@@ -165,6 +215,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         });
     });
 </script>
+
 <!-- //Categories -->
 <!--footer section start-->
 <footer>
@@ -173,7 +224,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             <div class="foo-grids">
                 <div class="col-md-3 footer-grid">
                     <h4 class="footer-head">Quem somos nós</h4>
+
                     <p>Estudantes do IFBA, bla bla bla bla.</p>
+
                     <p>Estudantes do IFBA, bla bla bla bla.</p>
                 </div>
                 <div class="col-md-3 footer-grid">
