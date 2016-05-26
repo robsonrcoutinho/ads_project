e03ea939355a5cd0a77d169a2c616ddd982de964
Luana Almeida <almeidah.luana@gmail.com>
2016-05-26 18:02:34 -0300

    Aluno Migration

---
+++ database/migrations/2016_05_24_185234_create_table_aluno.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAluno extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('matricula', 6)->unique();
+            $table->string('nome', 30);
+            $table->string('email', 100)->nullable();
+            $table->softDeletes();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       // Schema::drop('alunos');
+ 
+    }
+}
