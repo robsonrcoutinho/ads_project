83f186f421d25a1d42201ffbcee7a301bb48348f
Luana Almeida <almeidah.luana@gmail.com>
2016-05-26 18:04:33 -0300

    AlunosController

---
+++ app/Http/Controllers/AlunosController.php
@@ -0,0 +1,43 @@
+<?php namespace adsproject\Http\Controllers;
+
+use adsproject\Aluno;
+use adsproject\Http\Requests\AlunoRequest;
+
+
+class AlunosController extends Controller{
+
+    public function index(){
+        $alunos = Aluno::all();
+        return view('alunos.index', ['alunos'=>$alunos]);
+    }
+     public function novo(){
+        return view('alunos.novo');
+    }
+
+    public function salvar(AlunoRequest $request){
+        $aluno = $request->all();
+        Aluno::create($aluno);
+        return redirect()->route('alunos');
+    }
+    public function editar($id){
+        $aluno = Aluno::find($id);
+        return view('alunos.editar', compact('aluno'));
+    }
+    public function alterar(AlunoRequest $request, $id){
+        Aluno::find($id)->update($request->all());
+        return redirect()->route('alunos');
+    }
+    public function excluir($id){
+        Aluno::find($id)->delete();
+        return redirect()->route('alunos');
+    }
+
+    public function buscarTodos(){
+        return Aluno::all();
+    }
+    
+    public function buscarPorId($id){
+        return Aluno::find($id);
+    }
+
+}
