cf71fa7b5e46ef003b5552158da4b29fc35ff8a4
Wilder <wildermachadodacruz@gmail.com>
2016-05-26 10:28:34 -0300

    Alteração tela novo de Questionarios

--- resources/views/questionarios/novo.blade.php
+++ resources/views/questionarios/novo.blade.php
@@ -15,16 +15,14 @@
             <fieldset>
                 <ol id="perguntas">
                     <legend>Perguntas</legend>
-
                     @foreach($avaliacao->perguntas as $pergunta)
                         <li class="collection-item">
                             {!! Form::label($pergunta->id, $pergunta->enunciado) !!}
                             @if($pergunta->pergunta_fechada)
-                                <br/>
                                 @foreach($pergunta->opcoes_resposta as $opcao)
+                                    <br/>
                                     {!! Form::radio("campo_resposta[$pergunta->id]", $opcao->resposta_opcao, null,['id'=>$opcao->id, 'class'=>'with-gap']) !!}
                                     {!! Form::label($opcao->id, $opcao->resposta_opcao) !!}
-                                    <br/>
                                 @endforeach
                             @else
                                 {!! Form::text("campo_resposta[$pergunta->id]", null, ['class'=>'form-control']) !!}
