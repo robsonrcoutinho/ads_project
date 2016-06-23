702a8bc3de4fba01ef1a390f509bee1d61117736
Wilder <wildermachadodacruz@gmail.com>
2016-06-23 10:35:15 -0300

    Alteração de users/editar.blade.php

--- resources/views/users/editar.blade.php
+++ resources/views/users/editar.blade.php
@@ -2,10 +2,10 @@
 @section('conteudo')
     <div class="contegory">
         <div class="card-panel  teal escurecer-4">
-            <span class=" grey-text text-lighten-5">Novo Usuário</span>
+            <span class=" grey-text text-lighten-5">Editar Usuário</span>
         </div>
         {!! Form::open(['route'=>['users.alterar', $user->id], 'method'=>'put']) !!}
-         {!! csrf_field() !!}
+        {!! csrf_field() !!}
         <div class="form-group">
             {!! Form::hidden ('id', $user->id, ['class'=>'form-control']) !!}
         </div>
@@ -17,12 +17,16 @@
             {!! Form::label ('email', 'E-mail: ') !!}
             {!! Form::email ('email', $user->email, ['class'=>'form-control']) !!}
         </div>
-        @can()
+
         <div class="form-group">
+            @can('alterar', $user)
             {!! Form::label ('role', 'Papel: ') !!}
             {!! Form::select ('role', $roles, $user->role, ['class'=>'browser-default']) !!}
+            @else
+                {!! Form::hidden ('role', $user->role, ['class'=>'form-control']) !!}
+                @endcan
         </div>
-        @endcan
+
         <div class="form-group">
             {!! Form::label ('password', 'Senha: ') !!}
             {!! Form::password ('password', ['class'=>'form-control']) !!}
