b725f2dce590a1d17c4f89760a497eef5d23fb10
Luana Almeida <almeidah.luana@gmail.com>
2016-07-29 03:03:13 -0300

    Welcome Temporaria

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -76,6 +76,8 @@
 <div class="container">
     <div class="col 10 m10">
 
+
+
         <div class="col m10 ">
 
             @if($errors->any())
@@ -85,7 +87,34 @@
                     @endforeach
                 </ul>
             @endif
-            @yield('conteudo')
+            @yield('conteudo','
+
+            <div class="container">
+                       <div><h5 class="center-align">Bem vinda(o) ao Portal</h5></div>
+                       <div class="center-align">
+                        Esse sistema permite acesso a diversas informações relacionadas ao curso de
+                        Tecnologia em Análise e Desenvolvimento de Sistema oferecido pelo Campus Eunápolis
+                        do Instituto Federal de Educação, Ciência e Tecnologia do Estado da Bahia
+                        Tais como:<br>
+                        <i class="material-icons">done</i>Calendário Acadêmico
+                        <i class="material-icons">done</i> Grade Curricular
+                        <i class="material-icons">done</i>Horário de Aula
+                        <i class="material-icons">done</i>Normas Acadêmicas
+                        <i class="material-icons">done</i>Projeto Pedagógico
+                        <i class="material-icons">done</i>Avisos relacionados ao curso
+                        <i class="material-icons">done</i> Lista de professores<br>
+                        <i class="material-icons">done</i> Além de permitir aos alunos realizarem avaliação semestral
+                        </div>
+            </div>
+
+
+
+
+
+
+
+            ')
+
         </div>
 
         <br>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+@section('conteudo')
+    <div class="container">
+
+        <div><h5 class="center-align">Bem vinda(o) ao Portal</h5></div>
+        <div class="center-align">
+            Esse sistema permite acesso a diversas informações relacionadas ao curso de
+            Tecnologia em Análise e Desenvolvimento de Sistema oferecido pelo Campus Eunápolis
+            do Instituto Federal de Educação, Ciência e Tecnologia do Estado da Bahia
+            Tais como:<br>
+            <i class="material-icons">done</i>Calendário Acadêmico
+            <i class="material-icons">done</i> Grade Curricular
+            <i class="material-icons">done</i>Horário de Aula
+            <i class="material-icons">done</i>Normas Acadêmicas
+            <i class="material-icons">done</i>Projeto Pedagógico
+            <i class="material-icons">done</i>Avisos relacionados ao curso
+            <i class="material-icons">done</i> Lista de professores<br>
+            <i class="material-icons">done</i> Além de permitir aos alunos realizarem avaliação semestral
+        </div>
+@endsection
