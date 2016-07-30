8216e5708b80975b7a76c9dca100374f2736fb08
Wilder <wildermachadodacruz@gmail.com>
2016-07-30 17:15:53 -0300

    Teste API3

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -127,6 +127,7 @@ class ApiController extends Controller
     }
     public function buscaDisciplinasCursadas($email){
         $aluno=Aluno::query()->where('email',$email)->first();
+        dd($aluno);
          return response()
             ->json($aluno);
         //$disciplinas=$aluno->disciplinas()->get();
