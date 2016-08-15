bd8190529eb5067ee2968b623d434b88070aee00
Wilder <wildermachadodacruz@gmail.com>
2016-08-15 17:17:14 -0300

    Teste javascript

--- public/js/adsproject.js
+++ public/js/adsproject.js
@@ -10,7 +10,7 @@ window.onload = function () {
     }
     var pergunta_fechada = document.getElementById("pergunta_fechada");
     var escondida = document.getElementById('escondida');
-    if (!checked(pergunta_fechada)) {
+    if (pergunta_fechada) {
         escondida.style.display = 'none';
     } else {
         escondida.style.display = 'block';
