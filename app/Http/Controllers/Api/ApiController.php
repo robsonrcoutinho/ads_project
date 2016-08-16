7359ab24a4e1885da71887c97376c0b273442aaa
Wilder <wildermachadodacruz@gmail.com>
2016-08-16 09:37:48 -0300

    Teste ApiController 12

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -166,7 +166,7 @@ class ApiController extends Controller
     public function respostaQuestionario(Request $request)
     {
         //$respostas = json_decode($request->get('respostas'));
-        $tudo = json_decode($request->all());
+        $tudo = json_encode($request->all());
         $email = $tudo->email;
         $respostas = $tudo->respostas;
         $avaliacao = null;
