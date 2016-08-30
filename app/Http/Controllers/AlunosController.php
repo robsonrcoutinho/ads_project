174887f582b67df2f31cab587565086232eaf78e
Wilder <wildermachadodacruz@gmail.com>
2016-08-30 09:41:15 -0300

    Correção AlunosController (teste)

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -202,9 +202,9 @@ class AlunosController extends Controller
             //Apaga lista de alunos que não consta no arquivo
             Aluno::destroy(Aluno::all()->except($alunos)->lists('id')->toArray());
         } catch (\Exception $e) {                                       //Caso ocorra um excessão
+            dd($e, $alunos, $cont);
             return false;                                               //Retorna falso (false)
         }
-
         return true;                                                    //Retorna verdadeiro (true)
     }
 
